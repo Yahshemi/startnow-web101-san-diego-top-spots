fb13af737d6747004dfbdd0c3803a7f4edfb1820
yahshemi <yahshemi@gmail.com>
2022-12-20 00:10:32 -0800

    format files

--- main.js
+++ main.js
@@ -1,27 +1,17 @@
 $(document).ready(function () {
-    $.getJSON("./data.json", function (data) {
-        var items = [];
-        $.each(data, function (key, val) {
-            items.push("<tr><td>" + val.name + "</td></tr>" + val.description + "</td><td><a target='_blank' href='https://www.google.com/maps?q=" + val.location + "'>link</a></td></tr>");
-        });
-        $('table').append(items);
-
+  $.getJSON("./data.json", function (data) {
+    var items = [];
+    $.each(data, function (key, val) {
+      items.push(
+        "<tr><td>" +
+          val.name +
+          "</td></tr>" +
+          val.description +
+          "</td><td><a target='_blank' href='https://www.google.com/maps?q=" +
+          val.location +
+          "'>link</a></td></tr>"
+      );
     });
-    // write your code here
-
-    //Use the $.getJSON method to download the contents of the data.json file.
-    //Iterate through the top spots and create a table row for each spot.
-    //Create a link to the location using the longitude and latitude 
-    //provided. (Example https://www.google.com/maps?q=33.09745,-116.99572)
-    //jQuery.getJSON( "./data.json", [, data ] [, success ] )
-    //});
+    $("table").append(items);
+  });
 });
-
-
-
-
-   
-       
-       
-        
-   
